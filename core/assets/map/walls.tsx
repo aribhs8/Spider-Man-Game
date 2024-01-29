dfaf6cfdb20b4659208b2e168b814e479f0d0c3d
Arib Hussain <41489211+aribhs8@users.noreply.github.com>
2024-01-29 13:11:03 -0500

    Add files via upload

---
+++ core/assets/map/walls.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="walls" tilewidth="32" tileheight="32" tilecount="15" columns="15">
+ <image source="walls.png" width="480" height="32"/>
+</tileset>
